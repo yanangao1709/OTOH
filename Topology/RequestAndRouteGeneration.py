1394f97db47311837678ca472c3b41d90450c099
Yanan Gao <43428644+yanangao1709@users.noreply.github.com>
2023-07-29 00:22:54 +0800

    Add files via upload

---
+++ Topology/RequestAndRouteGeneration.py
@@ -0,0 +1,27 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#     Author: Yanan Gao                                       #
+#       Date: 28-07-2023                                      #
+#      Goals: request and candidate route generation                              #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+import HyperParameters
+import numpy as np
+import random
+
+class RequestAndRouteGeneration:
+    def __init__(self):
+        self.nodes_num = HyperParameters.topology_myself_nodes_num
+        self.requests = []
+        self.volumn_upper = 10
+        self.volumn_lower = 2
+
+    def request_generation(self):
+        source = random.randint(1,self.nodes_num)
+        destination = random.randint(1,self.nodes_num)
+        volumn = random.randint(self.volumn_lower, self.volumn_upper)
+
+
+
+
+
+
